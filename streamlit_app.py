9b11c345beaaa3d95915c4653654c8c1c7ec60fe
legit-dot <moniraa893@gmail.com>
2025-12-16 09:26:29 +0000

    switched to pro

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ if not api_key:
     st.stop()
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-pro')
 
 # --- TELEGRAM FUNCTION ---
 def send_to_telegram(image_bytes):
